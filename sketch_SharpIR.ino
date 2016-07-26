89145b5b8566ca9e44e1133b81499e0663b8e6bd
AdhiSekar <adhisek10@gmail.com>
2016-07-26 18:45:57 +0530

    Add files via upload

---
+++ sketch_SharpIR.ino
@@ -0,0 +1,15 @@
+
+int IRpin=1;
+int dist;
+int t;
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {  
+  dist = analogRead(IRpin); 
+  dist=12343.85 * pow(analogRead(IRpin),-1.15);
+  Serial.println(dist);            
+        
+}
